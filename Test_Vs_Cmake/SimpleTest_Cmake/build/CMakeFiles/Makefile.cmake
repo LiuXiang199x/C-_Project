e71959cb5478ecceac78d868128dd0088a8b5101
Marco <lx19961026@gmail.com>
2022-03-03 18:38:19 +0800

    nn

--- Test_Vs_Cmake/SimpleTest_Cmake/build/CMakeFiles/Makefile.cmake
+++ Test_Vs_Cmake/SimpleTest_Cmake/build/CMakeFiles/Makefile.cmake
@@ -47,6 +47,6 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "src/CMakeFiles/exc.dir/DependInfo.cmake"
+  "src/CMakeFiles/test_eigen.dir/DependInfo.cmake"
   "src/CMakeFiles/includeT.dir/DependInfo.cmake"
   )
---
+++ Test_Vs_Cmake/SimpleTest_Cmake/build/src/CMakeFiles/test_eigen.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "../../bin/test_eigen"
+  "../../bin/test_eigen.pdb"
+  "CMakeFiles/test_eigen.dir/includeT.cpp.o"
+  "CMakeFiles/test_eigen.dir/main.cpp.o"
+  "CMakeFiles/test_eigen.dir/test.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_eigen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
