d64f1059492e5bb87996fc6498f4fcbbcf82b7c7
Marco <lx19961026@gmail.com>
2022-03-07 17:42:40 +0800

    libs_rknn_opencv

---
+++ ONNX_RKNN/scene/build/CMakeFiles/rknn_class_infer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/rknn_class_infer"
+  "../bin/rknn_class_infer.pdb"
+  "CMakeFiles/rknn_class_infer.dir/src/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rknn_class_infer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
