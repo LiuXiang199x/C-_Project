7998a586702eee98a18d96115d771bec67aea106
Marco <lx19961026@gmail.com>
2022-03-25 17:22:48 +0800

    C-

---
+++ test_models_rknn/build/CMakeFiles/test_eigen.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/agent/C-_Project/test_models_rknn/src/main.cc" "/home/agent/C-_Project/test_models_rknn/build/CMakeFiles/test_eigen.dir/src/main.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/eigen3"
+  "/home/agent/C-_Project/Test_Vs_Cmake/SimpleTest_Cmake/librknn_api/include"
+  "/home/agent/rk_test_project/examples/libs/opencv/opencv410_aarch64/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- test_models_rknn/src/main.cc
+++ test_models_rknn/src/main.cc
@@ -1,5 +1,43 @@
+// #include "SceneNet.h"
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
 #include <iostream>
+#include "rknn_api.h"
+/*
+    one_hot_selected_objects[0] = one_hot_all_objects[0]
+    one_hot_selected_objects[1] = one_hot_all_objects[2]
+    one_hot_selected_objects[2] = one_hot_all_objects[4]
+    one_hot_selected_objects[3] = one_hot_all_objects[5]
+    one_hot_selected_objects[4] = one_hot_all_objects[6]
+    one_hot_selected_objects[5] = one_hot_all_objects[9]
+    one_hot_selected_objects[6] = one_hot_all_objects[11]
+    one_hot_selected_objects[7] = one_hot_all_objects[15]
+    one_hot_selected_objects[8] = one_hot_all_objects[16]
+    one_hot_selected_objects[9] = one_hot_all_objects[17]
+    one_hot_selected_objects[10] = one_hot_all_objects[18]
+    one_hot_selected_objects[11] = one_hot_all_objects[21]
+    one_hot_selected_objects[12] = one_hot_all_objects[23]
 
+    ['bed_room', 'dining_room', 'drawing_room', 'others', 'toilet']
+*/
 
+/*
+["bed"-0, "cabinet"-1, "chair_base"-2, "cupboard"-3, "dining_table"-4, 
+"door_anno"-5, "refrigerator"-6, "sofa"-7, "tea_table"-8, "toilet"-9, 
+"TV_stand"-10, "metal_chair_foot"-11, "washing_machine"-12]
+*/
 
 using namespace std;
+
+int main()
+{
+	printf("hello world\n");
+	float tmp_Nano[13] = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	float tmp_Scene[512] = {1};
+
+    string url1 = "/tmp/test/img/val/bed_room/";
+
+
+	return 0;
+} 
