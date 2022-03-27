e7bc4813a70cdf65216d72549270c32c42826504
marco <lx19961026@gmail.com>
2022-03-27 22:17:49 +0800

    opencv_c

---
+++ C_opencv/src/main.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "opencv2/core.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/videoio.hpp"
+
+using namespace std;
+
+int main(){
+
+    printf("hello, world");
+    return 0;
+}
