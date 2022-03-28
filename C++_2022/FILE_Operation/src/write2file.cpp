28468d77328fad733fbd27dccde77262d556c554
Marco <lx19961026@gmail.com>
2022-03-28 18:06:40 +0800

    files

--- C++_2022/FILE_Operation/src/write2file.cpp
+++ C++_2022/FILE_Operation/src/write2file.cpp
@@ -8,7 +8,7 @@ int test() {
     //创建一个 fstream 类对象
     fstream fs;
     //将 test.txt 文件和 fs 文件流关联
-    fs.open("/home/agent/C-_Project/C++_2022/FILE_Operation/files/test2.txt");
+    fs.open("/home/agent/C-_Project/C++_2022/FILE_Operation/files/test2.txt", ios::out);
     //向test.txt文件中写入 url 字符串
     cout << sizeof(strrr) << " " << sizeof(strrr[0]) << endl;   // 8, 1
     cout << strlen(strrr) << endl;
