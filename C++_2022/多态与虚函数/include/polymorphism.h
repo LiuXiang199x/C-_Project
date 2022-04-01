2c0b1b1ffb0bdcbcd77ad03c4fc475020e0be25e
Marco <lx19961026@gmail.com>
2022-04-01 18:39:05 +0800

    encapsulation

--- C++_2022/多态与虚函数/include/polymorphism.h
+++ C++_2022/多态与虚函数/include/polymorphism.h
@@ -2,26 +2,26 @@
 #define _POLYMORPHISM_H_
 #include "allLibs.h"
 
-int test_polymorphism();
+// int test_polymorphism();
 
-class Shape {
-   protected:
-      int width, height;
+// class Shape {
+//    protected:
+//       int width, height;
       
-   public:
-      Shape( int a=0, int b=0);
-      int area();
-};
+//    public:
+//       Shape( int a=0, int b=0);
+//       int area();
+// };
 
-class Rectangle: public Shape{
-   public:
-      Rectangle( int a=0, int b=0):Shape(a, b) { }
-      int area ();
-};
-class Triangle: public Shape{
-   public:
-      Triangle( int a=0, int b=0):Shape(a, b) { }
-      int area ();
-};
+// class Rectangle: public Shape{
+//    public:
+//       Rectangle( int a=0, int b=0):Shape(a, b) { }
+//       int area ();
+// };
+// class Triangle: public Shape{
+//    public:
+//       Triangle( int a=0, int b=0):Shape(a, b) { }
+//       int area ();
+// };
 
 #endif // !_POLYMORPHISM_H_
