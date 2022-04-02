75a1586fef0df86424b02cbc903c7bf443fff662
Marco <lx19961026@gmail.com>
2022-04-02 12:09:46 +0800

    polumorphism

--- C++_2022/多态与虚函数/src/polymorphism.cpp
+++ C++_2022/多态与虚函数/src/polymorphism.cpp
@@ -3,39 +3,42 @@
 using namespace std;
 
 
-// Shape::Shape(int a=0, int b=0){
-//     width = a;
-//     height = b;
-// }
+Shape::Shape(int a, int b){
+    width = a;
+    height = b;
+}
 
-// int Shape::area(){
-//     cout << "Parent class area : " << endl;
-//     return 0;
-// }
+int Shape::area(){
+    cout << "Parent class area : 0000 -> (i am in parent class!!)" << endl;
+    return 0;
+}
 
+// Rectangle::Rectangle(int cc=1, int dd=2):Shape(cc, dd){
+// }  
+// 这里函数里面其实已经不用再详细写了，因为Shape::Shape里面帮你把内容定义好了
 
-// Rectangle::Rectangle(int a=0; int b=0):Shape(a, b){
-
-// }
-
-// int Rectangle::area(){
-//     cout << "Rectangle class area :" <<endl;
-//     return (width * height); 
-// }
-
-
-// class Triangle: public Shape{
-//    public:
-//       Triangle( int a=0, int b=0):Shape(a, b) { }
-//       int area ()
-//       { 
-//          cout << "Triangle class area :" <<endl;
-//          return (width * height / 2); 
-//       }
-// };
-
-// int test_polymorphism(){
+int Rectangle::area(){
+    cout << "Rectangle class area : " << width*height << " -> (i am in rectangle class!!)" <<endl;
+    return (width * height); 
+}
 
 
-//     return 0;
-// }
+int Triangle::area(){
+
+    cout << "Triangle class area : " << (width * height / 2) << " -> (i am in triangle class!!)" <<endl;
+
+    return (width * height / 2);
+}
+
+
+int test_polymorphism(){
+    Shape a(2,2);
+    Rectangle b;
+    Triangle c;
+
+    cout << "Shape: " << a.area() << endl;
+    cout << "Rectangle: " << b.area() << endl;
+    cout << "Triangle: " << c.area() << endl;
+
+    return 0;
+}
